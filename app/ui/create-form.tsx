9a13da91cc2107339eba2b24f6e5b675bcc48e23
paridhi7 <theparidhi0@gmail.com>
2024-04-18 02:05:08 +0530

    Accept only images in image input

--- app/ui/create-form.tsx
+++ app/ui/create-form.tsx
@@ -78,6 +78,7 @@ export default function Form() {
             id="mockupImages"
             name="mockupImages"
             className="peer block w-full rounded-md border border-gray-200 py-2 pl-2 text-sm outline-2 placeholder:text-gray-500"
+            accept="image/*"
             required
             onChange={handleFileChange}
           />
