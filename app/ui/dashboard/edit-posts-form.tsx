faff17f710e5669a5269b6433fa3307cf84d999a
paridhi7 <theparidhi0@gmail.com>
2024-04-18 03:01:16 +0530

    Allow only image inputs in edit form

--- app/ui/dashboard/edit-posts-form.tsx
+++ app/ui/dashboard/edit-posts-form.tsx
@@ -93,6 +93,7 @@ export default function EditPostForm({ post }: { post: PostForm }) {
             name="mockupImages"
             className="peer block w-full rounded-md border border-gray-200 py-2 pl-2 text-sm outline-2 placeholder:text-gray-500"
             onChange={handleFileChange}
+            accept="image/*"
           />
         </div>
         {imageSrc && (
